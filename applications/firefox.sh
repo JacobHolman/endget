f2dd60595d905766e7591fc1d94976f2d04e2478
Lava <69821200+RealLava@users.noreply.github.com>
2023-08-29 01:29:14 -0400

    Create firefox.sh

---
+++ applications/firefox.sh
@@ -0,0 +1,4 @@
+wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+tar xjf firefox.tar.bz2
+sudo mv firefox /opt
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
